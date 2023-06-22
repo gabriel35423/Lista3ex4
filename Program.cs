7820de24b9cee3713394f4ac462f268396fb88e5
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-06-21 21:54:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 0;
+            int cont = 0;
+
+            do
+            {
+                cont++;
+                valor = 5 * cont;
+                Console.WriteLine("5 x {0} = {1}", cont, valor);
+            } while (cont < 10);
+
+        }
+    }
+}
